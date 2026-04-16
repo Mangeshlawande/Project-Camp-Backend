29c87da7cbe2919ff51ce8786c730cea4dbae0b3
Mangeshlawande <mangeshlawande223@gmail.com>
2026-04-16 22:08:01 +0530

    database Connection

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import mongoose from "mongoose";
+import logger from "./logger.js";
+
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        logger.info("✅ MongoDB connected")
+    } catch (error) {
+        logger.error("❌ MongoDB connection error", error)
+
+        process.exit(1);
+    }
+};
+
+export default connectDB;
