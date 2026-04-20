ce538ec317f84e5303b57d3deae5566533a98453
Mangeshlawande <mangeshlawande223@gmail.com>
2026-04-20 21:32:50 +0530

    configure user model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,62 @@
+import  { Schema } from "mongoose";
+
+ export const userSchema = new Schema(
+    {
+        avatar: {
+            type: {
+                url: String,
+                localPath: String,
+            },
+            default: {
+                url: `https://placehold.co/200x200`,
+                localPath: "",
+            }
+        },
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            required: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPassowrdExpiry: {
+            type: Date,
+        },
+        emailVerificationToken: {
+            type: String,
+        },
+        emailVerificationExpiry: {
+            type: Date,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
